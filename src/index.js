7703c69338ffbdfd35b0ac6d15f0d2236aa13ffc
Bruno Montebello <brunormontebello@hotmail.com>
2020-10-20 19:53:49 +0100

    testing http methods

--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ const express = require('express');
 
 const app = express();
 
-app.get('/projects', (request, response) => {
-  return response.json({ message: 'Hello World'});
+//metodos HTTP
+//GET: le/busca infos do backend
+//POST: cria infos no backend
+//PUT/PATCH: altera infos no backend. Put em bloco; Patch de forma especifica
+
+app.get('/projects', (request, response) => { //o que vem após a / chama-se recurso/resource
+  return response.json([
+    'prjeto 1',
+    'prjeto 2'
+  ]);
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log('Back-end started');
+});
